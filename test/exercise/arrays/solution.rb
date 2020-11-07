4d9fe5cae0134a32434921e92073d623e36e3e91
Nikita Konovalov <mipo4t@gmail.com>
2020-11-07 17:39:14 +0400

    add search method solution

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -6,8 +6,18 @@ module Exercise
         array.map { |item| item.positive? ? max : item }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        iter = lambda do |first, last|
+          return -1 unless first <= last
+
+          mid = (first + last) / 2
+
+          return mid if array[mid] == query
+          return iter.call(first, mid - 1) if query < array[mid]
+          return iter.call(mid + 1, last) if query > array[mid]
+        end
+
+        iter.call(0, array.size - 1)
       end
     end
   end
