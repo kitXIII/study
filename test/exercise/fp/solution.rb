beda5a073289d71fd5f052c2ab314da413a1e018
Nikita Konovalov <mipo4t@gmail.com>
2020-11-10 23:38:19 +0400

    add ratings solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,14 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        filtered_ratings = array.reject { |film| film['country'].nil? || film['country'].split(',').size < 2 }
+                                .map { |film| film['rating_kinopoisk'].to_f }
+                                .reject(&:zero?)
+
+        return 0 if filtered_ratings.empty?
+
+        filtered_ratings.reduce(:+) / filtered_ratings.size
       end
 
       def chars_count(_films, _threshold)
