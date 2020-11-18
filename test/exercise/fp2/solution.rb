4ca540a7cfa0eb805a10b28fea525929a0bce2d5
Nikita Konovalov <mipo4t@gmail.com>
2020-11-18 23:35:12 +0400

    add array methods solution

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,43 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each(&block)
+        iter = lambda do |index|
+          return if index >= size
+
+          block.call(self[index].dup)
+          iter.call(index + 1)
+        end
+
+        iter.call(0) unless block.nil?
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map(&block)
+        my_reduce(self.class.new) { |acc, el| acc << block.call(el) }
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        my_reduce(self.class.new) { |acc, el| el.nil? ? acc : acc << el }
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(init = nil, &block)
+        acc = init.nil? ? self[0].dup : init
+        start_index = init.nil? ? 1 : 0
+
+        iter = lambda do |index|
+          return if index >= size
+
+          acc = block.call(acc, self[index].dup)
+          iter.call(index + 1)
+        end
+
+        iter.call(start_index) unless block.nil?
+        acc
+      end
     end
   end
 end
