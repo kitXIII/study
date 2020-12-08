872f63a69b6ad423f30f0ba51030434c9b7436e8
Nikita Konovalov <mipo4t@gmail.com>
2020-12-08 23:55:24 +0400

    fix solution by remark

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,21 +5,21 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each(&block)
-        iter = lambda do |index|
-          return if index >= size
+      def my_each(index = 0, &block)
+        return self if index >= size
 
-          block.call(self[index].dup)
-          iter.call(index + 1)
-        end
-
-        iter.call(0) unless block.nil?
-        self
+        yield self[index].dup if block_given?
+        my_each(index + 1, &block)
       end
 
       # Написать свою функцию my_map
-      def my_map(&block)
-        my_reduce(self.class.new) { |acc, el| acc << block.call(el) }
+      def my_map
+        return self unless block_given?
+
+        my_reduce(self.class.new) do |acc, item|
+          result = yield item
+          acc << result
+        end
       end
 
       # Написать свою функцию my_compact
@@ -28,18 +28,11 @@ module Exercise
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce(init = nil, &block)
+      def my_reduce(init = nil)
         acc = init.nil? ? self[0].dup : init
         start_index = init.nil? ? 1 : 0
 
-        iter = lambda do |index|
-          return if index >= size
-
-          acc = block.call(acc, self[index].dup)
-          iter.call(index + 1)
-        end
-
-        iter.call(start_index) unless block.nil?
+        my_each(start_index) { |item| acc = yield acc, item }
         acc
       end
     end
